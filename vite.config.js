aa9a2c7c108ea21501dbaec8a87edfe4b5b4e5c5
j-tap <j-tap@ya.ru>
2024-09-28 13:43:54 -0500

    fix (config): fixed manifest options

--- vite.config.js
+++ vite.config.js
@@ -24,10 +24,24 @@ export default defineConfig({
           swSrc: 'dev/sw.js',
           runtimeCaching: [
             {
-              urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp|woff)$/,
+              urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/,
               handler: 'CacheFirst',
               options: {
                 cacheName: 'images',
+                expiration: {
+                  maxEntries: 50,
+                  maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+                },
+                cacheableResponse: {
+                  statuses: [0, 200]
+                }
+              }
+            },
+            {
+              urlPattern: /\.(?:woff|woff2)$/,
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'fonts',
                 expiration: {
                   maxEntries: 50,
                   maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
