ff7f4f4e9eb0b746d1186e11119e8318d9e972d8
j-tap <j-tap@ya.ru>
2025-07-15 21:40:15 -0500

    chore (client): added widget vr9

--- client/src/components/TheFooter.vue
+++ client/src/components/TheFooter.vue
@@ -1,14 +1,17 @@
-<template lang="pug">
-v-footer.flex-grow-0.text-grey-darken-1
-  v-row(justify="center" no-gutters)
-    span.text-caption
-      span ©&nbsp;
-      a.no-link(href="https://vr9.pro" target="_blank") VR Nine
-      span &nbsp;2024 — {{ year }}
-      span &nbsp;[Build: &sharp;{{ commitHash }}]
-</template>
-
 <script setup>
 const commitHash = __COMMIT_HASH__
 const year = new Date().getFullYear()
 </script>
+
+<template lang="pug">
+v-footer.flex-grow-0.text-grey-darken-1
+  v-row(justify="space-between" no-gutters)
+    span &nbsp;
+    span.text-caption
+      span ©&nbsp;
+      span &nbsp;2024 — {{ year }}
+      span &nbsp;[Build: &sharp;{{ commitHash }}]
+    span.vr9copy
+      |Created by&nbsp;
+      a(href="https://vr9.pro" target="_blank") VR nine
+</template>
